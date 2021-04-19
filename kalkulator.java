31b15a743aefdcd17511550a2a90cce78310457d
Friski <kiki30632@gmail.com>
2021-04-19 22:10:42 +0700

    kalkulator

--- kalkulator.java
+++ kalkulator.java
@@ -2,11 +2,26 @@
 /**
  * Write a description of class kalkulator here.
  *
- * @author (your name)
+ * @author Friski
  * @version (a version number or a date)
  */
 public class kalkulator
 {
+    string name;
+    public int tambah (int x, int y){
+        return x + y
+    }
     
+    public int kurang (int x, int y){
+        return x - y
+    }
+    
+    public int kali (int x, int y){
+        return x * y
+    }
+    
+    public int bagi (int x, int y){
+        return x / y
+    }
     
 }
