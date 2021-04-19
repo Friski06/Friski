a8419cbc04590eb6e79d3f3df07155b54edc0db4
Friski <kiki30632@gmail.com>
2021-04-20 05:28:55 +0700

    main

--- Main.java
+++ Main.java
@@ -12,12 +12,12 @@ public class Main{
             
             kalkul.name = "operator ";
             
-            system.out.println("nilai 1 = "+ kalkul.x);
-            system.out.println("nilai 1 = "+ kalkul.y);
+            System.out.println("nilai 1 = "+ kalkul.x);
+            System.out.println("nilai 1 = "+ kalkul.y);
             
-            system.out.println(kalkul.name + "tambah 6 + 4 = "+ kalkul.tambah);
-            system.out.println(kalkul.name + "kurang 6 - 4 = "+ kalkul.kurang);
-            system.out.println(kalkul.name + "kali 6 * 4 = "+ kalkul.kali);
-            system.out.println(kalkul.name + "bagi 6 / 4 = "+ kalkul.bagi);
+            System.out.println(kalkul.name + "tambah 6 + 4 = "+ kalkul.tambah);
+            System.out.println(kalkul.name + "kurang 6 - 4 = "+ kalkul.kurang);
+            System.out.println(kalkul.name + "kali 6 * 4 = "+ kalkul.kali);
+            System.out.println(kalkul.name + "bagi 6 / 4 = "+ kalkul.bagi);
         }
     }
