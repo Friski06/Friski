21e51b447bb7d6300ebc01e2bec4ccdf5eb03a1c
Friski <kiki30632@gmail.com>
2021-04-19 22:15:50 +0700

    main

--- Main.java
+++ Main.java
@@ -7,15 +7,15 @@
  */
 public class Main{
    
-        public static void main (string[] args){
+        public static void main (string args[]){
             calculator kalkul = new calculator();
             
             kalkul.name = "oprator ";
             
-            system.out.println("nilai 1 = "kalkul.x);
-            system.out.println("nilai 1 = "kalkul.y);
+            system.out.println("nilai 1 = "+kalkul.x);
+            system.out.println("nilai 1 = "+kalkul.y);
             
-            system.out.println("perjumlahan dari 6 + 4 = "+ kalkul.tambah());
-            system.out.println("perkalian dari 6 * 4 = "+ kalkul.kali)());
+            system.out.println("perjumlahan dari 6 + 4 = "+ kalkul.tambah);
+            system.out.println("perkalian dari 6 * 4 = "+ kalkul.kali);
         }
     }
